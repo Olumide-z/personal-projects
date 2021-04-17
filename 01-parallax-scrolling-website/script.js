a6b345bc92c2be22ce23519ec0e7f984104286f6
Olumide <ilesanmiolumide10@gmail.com>
2021-04-17 02:03:15 +0100

    all projects

---
+++ 01-parallax-scrolling-website/script.js
@@ -0,0 +1,19 @@
+const stars = document.querySelector('#stars')
+const moon = document.querySelector('#moon')
+const mountainBehind = document.querySelector('#mountains_behind')
+const mountainFront = document.querySelector('#mountains_front')
+const text = document.querySelector('#text')
+const btn = document.querySelector('#btn')
+const header = document.querySelector('header')
+
+window.addEventListener('scroll', () =>{
+    let value = window.scrollY
+    stars.style.left = value * 0.25 + 'px';
+    moon.style.top = value * 1.05 + 'px';
+    mountainBehind.style.top = value * 0.5 + 'px';
+    mountainFront.style.top = value * 0 + 'px';
+    text.style.marginRight = value * 4 + 'px';
+    text.style.marginTop = value * 1.5 + 'px';
+    btn.style.marginTop = value * 1.5 + 'px';
+    header.style.top = value * 0.5 + 'px';
+})
